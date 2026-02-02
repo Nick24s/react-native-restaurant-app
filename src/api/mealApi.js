2d034cc21af56470f76a8eddf19ca05ff84991b1
Nick24s <nickmilenov@gmail.com>
2026-02-02 17:14:36 +0200

    show meal from api on details page

--- src/api/mealApi.js
+++ src/api/mealApi.js
@@ -4,6 +4,13 @@ export function getAll() {
     return api.get('/meals');
 }
 
+export function getOne(mealId){
+    if(!mealId) {
+        throw new Error('No Meal ID provided!');
+    }
+    return api.get(`/meals/${mealId}`);
+}
+
 export function getAllByCategoryId(categoryId) {
     if(!categoryId) {
         throw new Error('No Category ID provided!');
